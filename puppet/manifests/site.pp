b9ef8844b860cc94e0cc7d11bd67efaba50309b7
Carlos Pintado <carlosmpintado@gmail.com>
2014-05-16 01:22:34 -0300

    Added installation of ruby 2.0

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,2 +1,14 @@
-#include maestrodev-rvm
+
 class { 'rvm': }
+
+# Users with permissions to run rvm
+rvm::system_user { vagrant: ; }
+
+# Ruby versions to be installed
+rvm_system_ruby {
+  'ruby-2.0':
+    ensure      => 'present',
+    default_use => true;
+}
+
+
