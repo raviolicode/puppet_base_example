342ec55890b08f27ebc2c02feea5f6f1cb89ed13
Lucia Escanellas <raviolicode@gmail.com>
2014-05-15 23:08:48 -0300

    Initial commit

---
+++ puppet/manifests/production.pp
@@ -0,0 +1,33 @@
+# production.pp
+stage { 'req-install': before => Stage['rvm-install'] }
+
+class requirements {
+  group { "puppet": ensure => "present", }
+  exec { "apt-update":
+    command => "/usr/bin/apt-get -y update"
+  }
+
+  package {
+    ["git-core"]: 
+      ensure => installed, require => Exec['apt-update']
+  }
+}
+
+class installrvm {
+  include rvm
+  rvm::system_user { vagrant: ; }
+
+  if $rvm_installed == "true" {
+    rvm_system_ruby {
+      'ruby-1.9.3-p0':
+        ensure => 'present';
+    }
+  }
+}
+
+class doinstall {
+  class { requirements:, stage => "req-install" }
+  include installrvm
+}
+
+include doinstall
